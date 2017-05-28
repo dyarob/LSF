87b23d501f05d4a5a77390817025190a2638fa2b
dyarob <dyarob@gmail.com>
2017-05-28 11:37:47 +0200

    -

--- game.js
+++ game.js
@@ -11,23 +11,28 @@ var width = (window.innerWidth || root.clientWidth || body.clientWidth) - 2,
 c.width = width;	c.height = height;
 ctx.translate(0.5, 0.5); //drawing lines exactly 1px wide
 ctx.fillStyle = 'white';
+
 // === GLOBALS ===
 var MAXX = 50, MAXY = 40,
 	gameTime = 8*60000, dt = 0, newDate = lastDate = (new Date()).valueOf();
+
 // === Globals for Components ===
-var name = new Map([[1,'Charlotte'], [2,'Cherry'], [3,'Vivian'], [4,'Patricia'],
+var nam = new Map([[1,'Charlotte'], [2,'Cherry'], [3,'Vivian'], [4,'Patricia'],
 		[5,'Marjory'], [6,'Francoise'], [7,'Kalypso'], [8,'Sofie'],
 		[9,'plantation-choux'], [10,'lounge']]),
 	pos = new Map([[1, {x:0, y:2}], [2, {x:3, y:2}], [3, {x:2, y:4}], [4, {x:4, y:5}],
-		[5, {x:4, y:12}], [6, {x:12, y:3}], [7, {x:5, y:8}], [8, {x:8, y:6}]]),
-	schedule = new Map([[1, []]]);
+		[5, {x:4, y:12}], [6, {x:12, y:3}], [7, {x:5, y:8}], [8, {x:8, y:6}]]);
+
+
 
 // === Jobs ===
+function	job(j, arg) {
+	return function() { j(arg); };
+}
+var jobs = new Set([job(walk, 1), job(walk, 2), job(walk, 3), job(walk, 4),
+	job(walk, 5), job(walk, 6), job(walk, 7), job(walk, 8)]);
 function	runJobs() {
 	jobs.forEach(function(job) { job(); });
-}
-function	wrapJob(j, arg) {
-	return function() { j(arg); };
 }
 function	walk(ID) {
 	var tmp = pos.get(ID),
@@ -36,10 +41,25 @@ function	walk(ID) {
 	pos.set(ID, { x: nxt.x<=0? 0: nxt.x>=MAXX? MAXX-1: nxt.x,
 			y: nxt.y<=0? 0: nxt.y>=MAXY? MAXY-1: nxt.y });
 }
-var jobs = new Set([wrapJob(walk, 1), wrapJob(walk, 2), wrapJob(walk, 3), wrapJob(walk, 4),
-	wrapJob(walk, 5), wrapJob(walk, 6), wrapJob(walk, 7), wrapJob(walk, 8)]);
+
+
 
 // === Event handler ===
+var	clickMap = new Array();
+for (let i = 0; i<~~(height/10); ++i) {
+	clickMap[i] = new Array();
+	for (let j = 0; j<~~(width/10); ++j) {
+		clickMap[i][j] = 0;
+	}
+}
+pos.forEach(function(p, ID) {
+		let tmp = toGrid(p);
+		clickMap[~~(tmp.x/10)][~~(tmp.y/10)] = ID; });
+c.addEventListener('click', function(event) {
+		if (clickMap[~~(event.pageX/10)][~~(event.pageY/10)]) {
+			//open entity contextual menu
+		}
+	}, false);
 /*window.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
 	// --- LEFT ---
@@ -48,55 +68,20 @@ var jobs = new Set([wrapJob(walk, 1), wrapJob(walk, 2), wrapJob(walk, 3), wrapJo
 	case 72: // h
 		Left();
     break;
-	// --- RIGHT ---
-    case 39: // Right
-    case 68: // d
-	case 76: // l
-		Right();
-    break;
-	// --- UP ---
-    case 38: // Up
-    case 87: // w
-	case 75: // k
-		Up();
-    break;
-	// --- DOWN ---
-    case 40: // Down
-    case 83: // s
-	case 74: // j
-		Down();
-    break;
-	// --- A ---
-	case 96:  // numpad 0
-	case 188: // ,
-	case 13:  // enter
-		A();
-	break;
-	// --- B ---
-	case 110: // numpad .
-	case 190: // period .
-	case 27:  // escape
-		B();
-	break;
   }
   cursor.context.setPosition();
 }, false);*/
-
 // --- METHODS ---
 // === !Event handler ===
 
+
+
 // === drawing ===
-//var BGimg = new Image();
-//BGimg.src = "BG.png";
-/*var drawBG = function(){
-	try {
-		ctx.drawImage(BGimg, 0, 0, 320, 288, 0, 0, 320, 288);
-	} catch (e) {}
-}*/
 function toGrid(pos) {
 	return {x:pos.x*width*.01+width*.2+width*.1/MAXY*(MAXY-pos.y), y:pos.y*height*.01+height*.3};
 }
 function drawChars() {
+	ctx.fillStyle = 'magenta';
 	ctx.beginPath();
 	let tmp = {x:0, y:0};
 	pos.forEach(function(pos, ID) {
@@ -106,12 +91,15 @@ function drawChars() {
 		ctx.lineTo(tmp.x+width*.008, tmp.y+height*.01);
 		ctx.lineTo(tmp.x-width*.002, tmp.y+height*.01);
 		ctx.lineTo(tmp.x, tmp.y);
+		ctx.fillText(nam.get(ID), tmp.x+width*.010, tmp.y-height*.006, 100);
 	});
 	ctx.strokeStyle = 'magenta';
 	ctx.stroke();
 	ctx.closePath();
+	ctx.fillStyle = 'white';
 }
 function drawShop() {
+	ctx.fillStyle = 'green';
 	ctx.beginPath();
 	var tmp = toGrid({x:9, y:4});
 	ctx.moveTo(tmp.x, tmp.y);
@@ -122,6 +110,7 @@ function drawShop() {
 	ctx.strokeStyle = 'green';
 	ctx.stroke();
 	ctx.closePath();
+	ctx.fillText(nam.get(9), tmp.x, tmp.y-height*.006, 100);
 	ctx.beginPath();
 	var tmp = toGrid({x:13, y:15});
 	ctx.moveTo(tmp.x, tmp.y);
@@ -132,6 +121,8 @@ function drawShop() {
 	ctx.strokeStyle = 'green';
 	ctx.stroke();
 	ctx.closePath();
+	ctx.fillText(nam.get(10), tmp.x, tmp.y-height*.006, 100);
+	ctx.fillStyle = 'white';
 }
 function	drawGameClock() {
 	ctx.fillText(~~(gameTime/60000) +" O'Clock", width/2-50, 20, 100);
@@ -164,8 +155,8 @@ var GameLoop = function(){
 	ctx.stroke();
 	ctx.closePath();
 		//overlay
-	drawChars();
 	drawShop();
+	drawChars();
 		//gui
 	drawGameClock();
 
